aa7d25779feccff665f5c11d04597c7b3448453b
Madeleine Woo <madeleinewoo@sandiego.edu>
2022-04-25 23:39:59 -0700

    Can't have a static class for Entry class

--- Compress.java
+++ Compress.java
@@ -28,7 +28,7 @@ public class Compress
 					//call log file method
 					badinput = false;
 				}
-				catch(IOExcepetion e)
+				catch(IOException e)
 				{
 					badinput = true;
 					System.out.println(e.getMessage());
@@ -45,13 +45,13 @@ public class Compress
 			//TODO get size of NEW file
 
 			//ask user for rerun
-			System.out.println("Do you want to compress another file? (/"y/" or /"n/")");
+			System.out.println("Do you want to compress another file? (y or n)");
 			input = sc.nextLine();
 		
 			//validation	
 			while(!input.equalsIgnoreCase("y") && !input.equalsIgnoreCase("n"))
 				{
-					System.out.println("INCORRECT!!! Please put /"y/" or /"n/".");
+					System.out.println("INCORRECT!!! Please put y or n.");
 					input = sc.nextLine();
 				}
 
@@ -62,5 +62,50 @@ public class Compress
 				rerun = false;
 		}
 		while(rerun);		
+	}
+}
+
+private static class Entry<K, V>
+{
+	//the key
+	private final K key;
+    //the value
+	private V value;
+			 
+    /*Create a new key-value pair.
+	 * @param key The key
+     * @param value The value
+	 */
+	public Entry(K key, V value)
+	{
+		this.key = key;
+        this.value = value;
+    }
+						   
+    /* Return the key
+	 * @return The key
+	 */
+    public K getKey()
+    {
+		return key;
+	}
+									   
+	/* Return the value
+	 * @return The value
+     */
+    public V getValue()
+	{
+		return value;
+	}
+												   
+	/* Set the value
+	 * @param val New value
+     * @return Old value
+     */
+	public V setValue(V newVal)
+	{
+		V oldVal = value;
+		value = newVal;
+		return oldVal;
 	}
 }
---
+++ Entry.java
@@ -0,0 +1,51 @@
+/*TODO Description:
+ * Authors: Cassidy Spencer and Madeleine Woo
+ * Date: 4/25/2022
+ */
+
+private static class Entry<K, V>
+{
+	//the key
+	private final K key;
+	//the value
+	private V value;
+
+	/*Create a new key-value pair.
+	 * @param key The key
+	 * @param value The value
+	 */
+	public Entry(K key, V value)
+	{
+		this.key = key;
+		this.value = value;
+	}
+
+	/* Return the key
+	 * @return The key
+	 */
+	public K getKey()
+	{
+		return key;
+	}
+
+	/* Return the value
+	 * @return The value
+	 */
+	public V getValue()
+	{
+		return value;
+	}
+
+	/* Set the value
+	 * @param val New value
+	 * @return Old value
+	 */
+	public V setValue(V newVal)
+	{
+		V oldVal = value;
+		value = newVal;
+		return oldVal;
+	}
+
+}
+
