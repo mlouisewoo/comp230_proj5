2585fc2dde5e8b5ad6decb4b35048391c369aeb0
cassidyspen <cassidyspencer@sandiego.edu>
2022-04-27 15:20:14 -0700

    added HashTableChain starter code

--- HashTableChain.java
+++ HashTableChain.java
@@ -47,6 +47,45 @@ public class HashTableChain <K,V> implements KWHashMap<K,V>
 			value = newVal;
 			return oldVal;
 		}
-	
-	}
+      public String toString()
+        {
+          return key.toString() + " = " + value.toString();
+        }
+	  //************************************************************************
+      private LinkedList<Entry<K,V>>[]table;
+      private int numKeys;
+      private static final int CAPACITY = TDA;
+      private static final double LOAD_THRESHOLD = TDA;
+
+      //should we put entry class in HashTableChain like he has it?
+
+      public HashTableChain()
+      {
+        table = new LinkedList[CAPACITY];
+        numKeys = 0;
+      }
+
+      public HashTableChain(int cap)
+      {
+        table = new LinkedList[cap];
+        numKeys = 0;
+      }
+
+      public V get(Object key)
+      {
+        int index = key.hashCode() % table.length;
+        if(index<0)
+          index += table.length;
+        if(table[index] == null)
+          return null;
+        
+        for(Entry<K,V> nextItem:table[index])
+        {
+          if(nextItem.getKey().equals(key))
+          return nextItem.getValue();
+        }
+      
+		return null
+		}
+}
 
