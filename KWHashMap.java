78c5b056e24d89e376b3a487ff20b881b0bfa017
cassidyspen <cassidyspencer@sandiego.edu>
2022-04-27 15:36:13 -0700

    added interface

---
+++ KWHashMap.java
@@ -0,0 +1,13 @@
+//hash map interface
+public interface KWHashMap<K,V>
+{
+	public V get(Object key);
+
+	public boolean isEmpty();
+
+	public V put(K key, V value);
+
+	public V remove(Object key);
+
+	public int size();
+}
