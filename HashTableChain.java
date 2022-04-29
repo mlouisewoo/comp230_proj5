3ef34f3445ca53a79d192b55f2e5c9b9bd592f61
Madeleine Woo <madeleinewoo@sandiego.edu>
2022-04-29 15:03:35 -0700

    put, remove, size methods

--- HashTableChain.java
+++ HashTableChain.java
@@ -47,45 +47,122 @@ public class HashTableChain <K,V> implements KWHashMap<K,V>
 			value = newVal;
 			return oldVal;
 		}
-      public String toString()
+      	
+		public String toString()
         {
           return key.toString() + " = " + value.toString();
         }
+	}	
 	  //************************************************************************
-      private LinkedList<Entry<K,V>>[]table;
-      private int numKeys;
-      private static final int CAPACITY = TDA;
-      private static final double LOAD_THRESHOLD = TDA;
+    private LinkedList<Entry<K,V>>[]table;
+    private int numKeys;
+    private static final int CAPACITY = TDA;
+    private static final double LOAD_THRESHOLD = TDA;
 
-      //should we put entry class in HashTableChain like he has it?
+    public HashTableChain()
+    {
+      table = new LinkedList[CAPACITY];
+      numKeys = 0;
+    }
 
-      public HashTableChain()
-      {
-        table = new LinkedList[CAPACITY];
-        numKeys = 0;
-      }
+    public HashTableChain(int cap)
+    {
+      table = new LinkedList[cap];
+      numKeys = 0;
+    }
 
-      public HashTableChain(int cap)
-      {
-        table = new LinkedList[cap];
-        numKeys = 0;
-      }
-
-      public V get(Object key)
-      {
-        int index = key.hashCode() % table.length;
-        if(index<0)
-          index += table.length;
-        if(table[index] == null)
-          return null;
+    public V get(Object key)
+    {
+      int index = key.hashCode() % table.length;
+      if(index<0)
+        index += table.length;
+      if(table[index] == null)
+        return null;
         
-        for(Entry<K,V> nextItem:table[index])
-        {
-          if(nextItem.getKey().equals(key))
-          return nextItem.getValue();
-        }
+      for(Entry<K,V> nextItem:table[index])
+      {
+        if(nextItem.getKey().equals(key))
+        return nextItem.getValue();
+      }
       
-		return null
+	  return null
+	}
+	
+	/*return true if table contains no key-value mappings*/
+	public boolean isEmpty()
+	{
+		if(numKeys == 0)
+			return true;
+		else
+			return false;
+	}
+
+	/* Method put for class HashtableChain
+	 * key-value pair is insirted in the table
+	 * and numKeys is incremented. If the key is 
+	 * already in the table, its value is changed to the
+	 * new  val and numKeys is unchanged.
+	 *
+	 * @param key The key of item being inserted
+	 * @param value The value of the key
+	 * @return The old value associated with existing key;
+	 * otherwise, null
+	 */
+	public V put(K key, V Value) 
+	{
+		int index = key.hashCode % table.length;
+		if(index < 0)
+			index += table.length;
+		if(table[index] == null)
+		{
+			//create new linked list at table[index]
+			table[index] = newLinkedList<>();
+		}
+
+		//search the list at table[index] to find the key
+		for (Entry<K, V> nextItem : table[index])
+		{
+			//if the search is successful, replace the old value
+			if (nextItem.getKey().equals(key))
+			{
+				//replace value with this key
+				V oldVal = nextItem.getValue();
+				nextItem.setValue(value);
+				return oldVal;
+			}
+		}
+
+		//if key is not in the table, addd new item
+		table[index].addFirst(new Entry<>(key, value));
+		numKeys++;
+		if (numKeys > (LOAD_THRESHOLD * table.length))
+			rehash(); //TODO Rehash method? UGGH
+		return null;
+	}
+
+	public V remove(K key)
+	{
+		int index = key.hasCode() % table.length;
+		if(index < 0)
+			index += table.length;
+		
+		//key is not in the table
+		if(table[index] == null)
+			return null;
+		
+		//search the list at table[index] to find the key
+		for (Entry<K, V> nextItem : table[index])
+		{
+			//if we find the key, remove the entry
+			if (nextItem.getKey().equals(key))
+			{	
+				table[index].remove(nextItem);
+				numKeys--;
+				if(table[index].size == 0) //TODO trying to see if list is empty
+					table[index] = null;
+				return value; //TODO return value associated with the key 
+			}
+			return null;
 		}
 }
 
